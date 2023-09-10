375cbd3cc4cd85ce4571a4ac603742b3641e1de1
shunngerold <shunngerold@gmail.com>
2023-09-10 11:03:13 +0800

    Profile starting UI

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,12 +4,18 @@
     <x-header_links />
     <title>Profile | MyGrocery</title>
 </head>
-<body>
+<body class="bg-slate-50">
     {{-- Navigator --}}
     @include('partials._navigator')
-    
-    <div class="mt-28">
-        sdasdsaasdasdsa
+    <div class="mt-28 px-16 grid grid-cols-4 gap-4 min-h-screen">
+        <div class="p-5">
+            <div class="flex items-center justify-center">
+                <p class="text-lg font-semibold">Hello, {{ $profile->given_name }}</p>
+            </div>
+        </div>
+        <div class="col-span-3">
+            weq
+        </div>
     </div>
 
     {{-- Footer --}}
